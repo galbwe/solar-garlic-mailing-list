4db03dbac49306c3f0f5ba1565f6b58b0da70741
Wes Galbraith <galbwe92@gmail.com>
2025-06-11 23:20:41 -0600

    Make cors middleware configurable by environment varialbe

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
 	"github.com/robfig/cron/v3"
@@ -46,6 +47,7 @@ func main() {
 	EMAIL_VERIFICATION_SUCCESS_REDIRECT := setConfig("EMAIL_VERIFICATION_SUCCESS_REDIRECT", false)
 	EMAIL_VERIFICATION_ENDPOINT := setConfig("EMAIL_VERIFICATION_ENDPOINT", false)
 	EMAIL_UNSUBSCRIBE_ENDPOINT := setConfig("EMAIL_UNSUBSCRIBE_ENDPOINT", false)
+	ALLOW_ALL_CORS := setConfig("ALLOW_ALL_CORS", false)
 
 	// configure logging
 	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
@@ -61,14 +63,16 @@ func main() {
 	r.Use(middleware.Logger)
 
 	// CORS middleware
-	// r.Use(cors.Handler(cors.Options{
-	// 	AllowedOrigins:   []string{"https://*", "http://*"},
-	// 	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	// 	AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-	// 	ExposedHeaders:   []string{"Link"},
-	// 	AllowCredentials: false,
-	// 	MaxAge:           300, // Maximum value not ignored by any of major browsers
-	// }))
+	if ALLOW_ALL_CORS == "true" {
+		r.Use(cors.Handler(cors.Options{
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300, // Maximum value not ignored by any of major browsers
+		}))
+	}
 
 	var validate = validator.New(validator.WithRequiredStructEnabled())
 
